06a77cc77c285e734cc09e112e3781a3296557b0
twinky <padillaortiz.daniel099@gmail.com>
2025-07-23 00:35:37 -0700

    removed the comments on the listeners and removed update_source_msg function

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ from slack_bolt import App
 from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 from dotenv import load_dotenv
 load_dotenv()
 
-SLACK_APP_TOKEN= os.getenv("APP_TOKEN")
-SLACK_SIGNING_SECRET = os.getenv("SIGNING_SECRET")
-SLACK_BOT_TOKEN = os.getenv("BOT_TOKEN")
-SLACK_USER_TOKEN = os.getenv("USER_TOKEN")
+SLACK_APP_TOKEN= os.getenv("SLACK_APP_TOKEN")
+SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_USER_TOKEN = os.getenv("SLACK_USER_TOKEN")
+MOD_CHANNEL = os.getenv("MOD_CHANNEL")
 
 # https://api.slack.com/authentication/verifying-requests-from-slack
 # Initializes your app with your bot token and signing secret
@@ -28,6 +30,30 @@ app = App(
     token=SLACK_BOT_TOKEN,
     signing_secret=SLACK_SIGNING_SECRET
 )
+"""
+timestamps=[1752704688.297829, 1752788052.525229, 1752788621.798249]
+
+for ts in timestamps:
+    try:
+        app.client.chat_delete(
+            channel="D096402B45B",
+            ts=ts
+        )
+    except SlackApiError as e:
+        if e.response["error"] == "not_found":
+            print("Message not found, it may have already been deleted.")
+        else:
+            print(f"Error deleting message: {e.response['error']}")
+"""
+def get_todays_date() -> str:
+    """Returns today's date in the format %Y-%m-%d %I:%M:%S %p %Z."""
+    # Get the current datetime object
+    now = datetime.datetime.now()
+    # Convert to Pacific Time (Los Angeles)
+    pacific_timezone = ZoneInfo("America/Los_Angeles") 
+    now_aware = now.astimezone(pacific_timezone)
+    formatted_datetime = now_aware.strftime("%Y-%m-%d %I:%M:%S %p %Z")
+    return formatted_datetime
 
 def convert_epoch_timestamp(timestamp: float) -> str:
     """Converts a Unix timestamp to a human-readable 12-hour datetime string in Pacific Time (Los Angeles).
@@ -54,14 +80,6 @@ def blocks_message(emoji: str, user_id:str, ts:str) -> str:
         blocks["blocks"][1]["elements"][0]["value"]=emoji
     return json.dumps(blocks["blocks"])
 
-def update_source_msg(response_url:str, text:str) -> Response:
-    payload = {
-        "replace_original": "true",
-        "text": f"{text}"
-    }
-    response = requests.post(response_url, data=json.dumps(payload))
-    return response
-
 def get_actor_id(event_timestamp:str) -> str:
     '''
     Authorization
@@ -82,8 +100,6 @@ def get_actor_id(event_timestamp:str) -> str:
         if event.get("date_create") == event_timestamp:
             return event.get("actor").get("user").get("id")
         
-# Unhandled request ({'type': 'event_callback', 'event': {'type': 'emoji_changed', 'subtype': 'add'}})
-# [Suggestion] You can handle this type of event with the following listener function:
 @app.event({'type': 'emoji_changed', 'subtype': 'add'})
 def handle_emoji_changed_events(ack, body, event, client):
     ack()
@@ -92,66 +108,48 @@ def handle_emoji_changed_events(ack, body, event, client):
     actor_id = get_actor_id(event_timestamp=ts)
 
     client.chat_postMessage(
-        channel="C0923REDJ0Z",
+        channel=MOD_CHANNEL,
         text=f":{emoji}: was uploaded by <@{actor_id}>",
         blocks=blocks_message(emoji, actor_id, ts)
     )
 
-# Unhandled request ({'type': 'block_actions', 'action_id': 'remove_emoji'})
-# [Suggestion] You can handle this type of event with the following listener function:
 @app.action("remove_emoji")
 def handle_remove_button(ack, body, client):
     ack()
     trigger_id=body["trigger_id"]
     private_metadata={
         "emoji":body["actions"][0]["value"],
-        "user_id":re.findall(r"<@([^>]+)>", body["message"]["text"])[0]
+        "user_id":re.findall(r"<@([^>]+)>", body["message"]["text"])[0],
+        "message_ts":body["message"]["ts"],
+        "current_message":body["message"]["blocks"][0]["text"]["text"]
     }
     client.views_open(
         view=view_block(private_metadata),
         trigger_id=trigger_id
     )
-    # Moving this code block to views
-    """
-    ts=float(body["actions"][0]["action_ts"])
-    date=convert_epoch_timestamp(ts)
-    emoji=body["actions"][0]["value"]
-    actor_id=body["user"]["id"]
-    prev_message=body["message"]["text"]
-    user_id=re.findall(r"<@([^>]+)>", prev_message)[0]
-    new_message=f":x: `:{emoji}:` was removed by <@{actor_id}> on {date}"
-    client.admin_emoji_remove(
-        token=SLACK_USER_TOKEN,
-        name=emoji
-    )
-    # https://api.slack.com/interactivity/handling#updating_message_response
-    update_source_msg(body["response_url"], f"{prev_message}\n{new_message}")
 
-    client.chat_postMessage(
-        channel=user_id,
-        text=new_message
-    )
-    """
-# Unhandled request ({'type': 'event_callback', 'event': {'type': 'emoji_changed', 'subtype': 'remove'}})
-# [Suggestion] You can handle this type of event with the following listener function:
 @app.event({'type': 'emoji_changed', 'subtype': 'remove'})
 def handle_emoji_removal(ack):
     ack()
-    pass # Do nothing
+    pass
 
-# Unhandled request ({'type': 'view_submission', 'view': {'type': 'modal', 'callback_id': 'memes'}})
-# [Suggestion] You can handle this type of event with the following listener function:
 @app.view("memes")
 def handle_view_submission_events(ack, body, client, view, logger):
     ack()
+    today = get_todays_date()
     private_metadata=json.loads(view["private_metadata"])
+    justification=view["state"]["values"]["input_block"]["submit_button"]["value"]
+    text=f"Your emoji, :{private_metadata["emoji"]}:, was removed on {today}"
+    last_message = private_metadata["current_message"]
     client.admin_emoji_remove(
         token=SLACK_USER_TOKEN,
         name=private_metadata["emoji"]
     )
-    justification=view["state"]["values"]["input_block"]["submit_button"]["value"]
-    text=f"<@{body["user"]["id"]}> removed your emoji, :{private_metadata["emoji"]}:"
-
+    client.chat_update(
+        channel=MOD_CHANNEL,
+        ts=private_metadata["message_ts"],
+        text=f"{last_message}\n:x: `:{private_metadata['emoji']}:` was removed by <@{body['user']['id']}> on {today}\nJustification: {justification}",
+    )
     if justification == None:
         client.chat_postMessage(
             channel=private_metadata["user_id"],
@@ -160,7 +158,7 @@ def handle_view_submission_events(ack, body, client, view, logger):
     else:
         client.chat_postMessage(
             channel=private_metadata["user_id"],
-            text=f"{text} {justification}"
+            text=f"{text}\nJustification: {justification}"
         )
 
     logger.info(body)
